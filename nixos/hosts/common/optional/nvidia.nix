cc15c3320c37268b4e77a48527cad9e099e6d0ae
j <j@test.email>
2025-03-17 14:07:20 -0400

    enabled powerManagement to hopefully fix issues waking up from suspend

--- nixos/hosts/common/optional/nvidia.nix
+++ nixos/hosts/common/optional/nvidia.nix
@@ -12,7 +12,7 @@
   hardware.nvidia = {
     modesetting.enable = true;
     # Experimental fix for sleep issues. Set to true if needed.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     # Experimental option to turn off GPU when not in use. Set to true if wanted.
     powerManagement.finegrained = false;
     # Use Nvidia open source kernel module. Set to true if wanted.
