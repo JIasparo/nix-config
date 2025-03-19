3d923f1872aa9d4241ef8147c4367096252071de
j <j@test.email>
2025-03-19 17:17:49 -0400

    changed default.nix

--- nixos/hosts/nixos/stryder/default.nix
+++ nixos/hosts/nixos/stryder/default.nix
@@ -5,6 +5,8 @@
     ./hardware-configuration.nix
     ./../../common/core
     ./../../common/optional/cinnamon.nix
+    #./../../common/optional/regreet.nix
+    #./../../common/optional/hyprland.nix
     ./../../common/optional/nvidia.nix
     ./../../common/optional/steam.nix
 
