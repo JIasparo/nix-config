0a9f326f5b8eb6fcca42083754c6b0bc3bbaf733
j <j@test.email>
2025-03-19 19:30:44 -0400

    consolidated core modules

--- home-manager/users/common/core/default.nix
+++ home-manager/users/common/core/default.nix
@@ -1,7 +1,11 @@
 { ... }:
 
 {
-  imports = [
-    ./hm.nix
-  ];
+  config = {
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
+    # Read Home Manager release notes before changing this value.
+    home.stateVersion = "24.11";
+  };
 }
--- home-manager/users/common/core/hm.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # Read Home Manager release notes before changing this value.
-  home.stateVersion = "24.11";
-}
