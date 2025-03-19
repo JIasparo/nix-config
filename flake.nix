b9a723831640217137cf3da3a9e41bebebadb34e
j <j@test.email>
2025-03-19 19:28:47 -0400

    reformatted using nixfmt

--- flake.nix
+++ flake.nix
@@ -12,31 +12,40 @@
     stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-stable, home-manager, stylix, ... }: 
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    pkgs-stable = nixpkgs-stable.legacyPackages.${system};
-  in {
-    nixosConfigurations = {
-      stryder = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./nixos/hosts/nixos/stryder
-        ];
-        specialArgs = { inherit inputs pkgs-stable; };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      stylix,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        stryder = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./nixos/hosts/nixos/stryder
+          ];
+          specialArgs = { inherit inputs pkgs-stable; };
+        };
       };
-    };
 
-    homeConfigurations = {
-      j = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home-manager/users/j
-          inputs.stylix.homeManagerModules.stylix
-        ];
-        extraSpecialArgs = { inherit inputs pkgs-stable; };
+      homeConfigurations = {
+        j = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home-manager/users/j
+            inputs.stylix.homeManagerModules.stylix
+          ];
+          extraSpecialArgs = { inherit inputs pkgs-stable; };
+        };
       };
     };
-  };
 }
