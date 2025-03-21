abc6849f8a6cce64121f90d0b78e1f1ff96e399f
j <j@test.email>
2025-03-21 15:49:51 -0400

    Declaratively set GNOME DE settings and keybinds using home manager

--- home-manager/users/j/gnome.nix
+++ home-manager/users/j/gnome.nix
@@ -5,12 +5,76 @@
     dconf = {
       enable = true;
 
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      settings."org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          #
-        ];
+      # dconf watch /
+      # Terminal command to see what happens when you toggle options in GNOME.
+      settings = {
+        "org/gnome/desktop/interface" = {
+          clock-show-day = true;
+          clock-show-weekday = true;
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+        };
+
+        "org/gnome/mutter" = {
+          dynamic-workspaces = false;
+        };
+
+        "org/gnome/desktop/wm/preferences" = {
+          num-workspaces = 4;
+        };
+
+        # Delete Utilities folder
+        "org/gnome/desktop/app-folders/folders" = {
+          Utilities = [ ];
+        };
+
+        # Tiling keybinds
+        "org/gnome/desktop/wm/keybindings" = {
+          switch-windows = [ "<Alt>Tab" ];
+
+          switch-applications = [ "<Super>Tab" ];
+          switch-group = [ "<Super>grave" ];
+
+          cycle-windows = [ "<Super>Escape" ];
+          cycle-windows-backward = [ "<Super>Escape" ];
+
+          switch-to-workspace-1 = [ "<Super>1" ];
+          switch-to-workspace-2 = [ "<Super>2" ];
+          switch-to-workspace-3 = [ "<Super>3" ];
+          switch-to-workspace-4 = [ "<Super>4" ];
+
+          move-to-workspace-1 = [ "<Shift><Super>1" ];
+          move-to-workspace-2 = [ "<Shift><Super>2" ];
+          move-to-workspace-3 = [ "<Shift><Super>3" ];
+          move-to-workspace-4 = [ "<Shift><Super>4" ];
+
+          move-to-monitor-left = [ "<Shift><Super>Left" ];
+          move-to-monitor-right = [ "<Shift><Super>Right" ];
+        };
+
+        "org/gnome/mutter/keybindings" = {
+          toggle-tiled-left = [ "<Super>Left" ];
+          toggle-tiled-right = [ "<Super>Right" ];
+        };
+
+        # Setup my media keys
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          volume-mute = [ "AudioMute" ];
+          volume-down = [ "AudioLowerVolume" ];
+          volume-up = [ "AudioRaiseVolume" ];
+
+          stop = [ "AudioStop" ];
+          previous = [ "AudioPrev" ];
+          play = [ "AudioPlay" ];
+          next = [ "AudioNext" ];
+        };
+
+        # Enable GNOME extensions
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = with pkgs.gnomeExtensions; [
+          ];
+        };
       };
     };
   };
