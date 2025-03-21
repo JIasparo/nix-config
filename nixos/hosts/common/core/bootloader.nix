f268950dcf51599759a25ee0ad9cfc720d5a451a
j <j@test.email>
2025-03-21 15:53:19 -0400

    enabled option to delete all files in /tmp during boot

--- nixos/hosts/common/core/bootloader.nix
+++ nixos/hosts/common/core/bootloader.nix
@@ -4,6 +4,8 @@
   config = {
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    
+    boot.tmp.cleanOnBoot = true;
 
     boot.plymouth.enable = true;
   };
