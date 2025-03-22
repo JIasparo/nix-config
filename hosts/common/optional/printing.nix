af68e5ae6acaf1b6399aaaf729a96fe84f5268bd
j <j@test.email>
2025-03-22 15:03:28 -0400

    reformatted this module

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -2,11 +2,13 @@
 
 {
   config = {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
 
-    # Some printers might need additional drivers.
-    services.printing.drivers = with pkgs; [
-      # Add additional printer drivers here:
-    ];
+      # Some printers might need additional drivers.
+      drivers = with pkgs; [
+        # Add additional printer drivers here:
+      ];
+    };
   };
 }
--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -4,9 +4,12 @@
   config = {
     programs.steam = {
       enable = true;
+
       # Opens firewall ports to allow Steam Remote Play services.
       remotePlay.openFirewall = true;
+
       gamescopeSession.enable = true;
+      
       # These packages are added to the Steam compatibility tools.
       extraCompatPackages = with pkgs; [
         # Glorious Eggroll Proton fork.
