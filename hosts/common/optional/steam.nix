6e426b78e33ffa9837f5ed2adc01c2b2a9b83c39
j <j@test.email>
2025-03-23 00:30:14 -0400

    commented out the gamescopeSession. Added then commented out the option to enable xbox one controller drivers in steam.nix.

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -2,13 +2,16 @@
 
 {
   config = {
+    # Enable drivers for Xbox One controllers.
+    #hardware.xone.enable = true;
+
     programs.steam = {
       enable = true;
 
       # Opens firewall ports to allow Steam Remote Play services.
       remotePlay.openFirewall = true;
 
-      gamescopeSession.enable = true;
+      #gamescopeSession.enable = true;
       
       # These packages are added to the Steam compatibility tools.
       extraCompatPackages = with pkgs; [
