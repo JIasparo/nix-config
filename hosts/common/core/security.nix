37e211c17eb613d56e9f92d073066db4ffebb870
j <j@test.email>
2025-03-23 00:27:58 -0400

    added a polkit to the security module

--- hosts/common/core/security.nix
+++ hosts/common/core/security.nix
@@ -3,5 +3,26 @@
 {
   config = {
     security.rtkit.enable = true;
+    
+    security.polkit = {
+      enable = true;
+
+      # Grants reboot and poweroff permissions to users in the users group.
+      extraConfig = ''
+        polkit.addRule(function (action, subject) {
+          if (
+            subject.isInGroup("users") &&
+            [
+              "org.freedesktop.login1.reboot",
+              "org.freedesktop.login1.reboot-multiple-sessions",
+              "org.freedesktop.login1.power-off",
+              "org.freedesktop.login1.power-off-multiple-sessions",
+            ].indexOf(action.id) !== -1
+          ) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    };
   };
 }
