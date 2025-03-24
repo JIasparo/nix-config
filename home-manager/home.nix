5d977efbef235e772dd21c9208cabe12229ec97f
j <j@test.email>
2025-03-24 18:35:41 -0400

    added home.sessionVariables to home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,26 +40,14 @@
       # '';
     };
 
-    # Home Manager can also manage your environment variables through
-    # 'home.sessionVariables'. These will be explicitly sourced when using a
-    # shell provided by Home Manager. If you don't want to manage your shell
-    # through Home Manager then you have to manually source 'hm-session-vars.sh'
-    # located at either
-    #
-    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  /etc/profiles/per-user/j/etc/profile.d/hm-session-vars.sh
-    #
     home.sessionVariables = {
-      # EDITOR = "emacs";
+      # Force apps to use Wayland
+      NIXOS_OZONE_WL = "1";
+      
+      #EDITOR = "codium";
+      TERM = "kitty";
     };
-    
+
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
