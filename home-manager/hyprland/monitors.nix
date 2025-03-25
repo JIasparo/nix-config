c693ded6bd88946ec437d2216036c5e2ddcefab9
j <j@test.email>
2025-03-25 00:02:09 -0400

    added comment explaining monitor rotations

--- home-manager/hyprland/monitors.nix
+++ home-manager/hyprland/monitors.nix
@@ -3,6 +3,17 @@
 {
   config = {
     wayland.windowManager.hyprland.settings = {
+      /*
+        Rotate a monitor: "..., transform, X" where x =
+        0 -> normal (no transforms)
+        1 -> 90 degrees
+        2 -> 180 degrees
+        3 -> 270 degrees
+        4 -> flipped
+        5 -> flipped + 90 degrees
+        6 -> flipped + 180 degrees
+        7 -> flipped + 270 degrees
+      */
       monitor = [
         "DP-1, 1920x1080@144, 2560x0, 1"
         "DP-2, 2560x1440@144, 0x0, 1"
