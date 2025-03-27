ce599864dc5808dc6f667efb74fb7c15e7f4686b
j <j@test.email>
2025-03-27 00:06:02 -0400

    changed settings.nix

--- home-manager/floorp/settings.nix
+++ home-manager/floorp/settings.nix
@@ -40,18 +40,79 @@
       };
       profiles =
         let
+          sharedSearch = {
+            force = true;
+            default = "Startpage";
+            privateDefault = "Startpage";
+          };
+
           sharedSettings = {
+            "browser.bookmarks.addedImportButton" = false;
+            "browser.bookmarks.restore_default_bookmarks" = false;
+            "browser.formfill.enable" = true;
+            "browser.newtabpage.activity-stream.floorp.background.type" = 0;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.search.separatePrivateDefault" = false;
+            "browser.startup.page" = 1;
+            "browser.uiCustomization.state" =
+              "{\"placements\":{\"widget-overflow-fixed-list\":[\"profile-manager\"],\"unified-extensions-area\":[\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"home-button\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"statusBar\":[\"screenshot-button\",\"fullscreen-button\",\"status-text\"]},\"seen\":[\"developer-button\",\"sidebar-reverse-position-toolbar\",\"undo-closed-tab\",\"profile-manager\",\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"statusBar\",\"unified-extensions-area\",\"PersonalToolbar\",\"TabsToolbar\",\"toolbar-menubar\",\"widget-overflow-fixed-list\"],\"currentVersion\":20,\"newElementCount\":3}";
+            "browser.urlbar.placeholderName.private" = "Startpage";
+            "browser.urlbar.placeholderName" = "Startpage";
+            "browser.urlbar.shortcuts.bookmarks" = false;
+            "browser.urlbar.shortcuts.history" = false;
+            "browser.urlbar.shortcuts.tabs" = false;
+            "browser.urlbar.suggest.bookmark" = false;
+            "browser.urlbar.suggest.clipboard" = false;
+            "browser.urlbar.suggest.engines" = false;
+            "browser.urlbar.suggest.history" = false;
+            "browser.urlbar.suggest.openpage" = false;
+            "browser.urlbar.suggest.recentsearches" = false;
+            "browser.urlbar.suggest.topsites" = false;
+            "extensions.formautofill.addresses.enabled" = false;
+            "extensions.formautofill.creditCards.enabled" = false;
+            "floorp.browser.sidebar.enable" = false;
+            "floorp.browser.workspaces.enabled" = false;
+            "floorp.download.notification" = 1;
+            "floorp.tabsleep.enabled" = true;
+            "general.autoScroll" = true;
+            "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
+            "places.history.enabled" = true;
+            "privacy.clearOnShutdown_v2.cache" = true;
+            "privacy.clearOnShutdown_v2.cookiesAndStorage" = true;
+            "privacy.donottrackheader.enabled" = true;
+            "privacy.globalprivacycontrol.enabled" = true;
+            "privacy.sanitize.pending" =
+              "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"siteSettings\",\"historyFormDataAndDownloads\",\"cookiesAndStorage\"],\"options\":{}}]";
+            "services.sync.prefs.sync.floorp.browser.note.memos" = false;
+            "signon.rememberSignons" = false;
+            "userChrome.autohide.back_button" = true;
+            "userChrome.autohide.forward_button" = true;
+            "webgl.disabled" = true;
+          };
+        in
+        {
+          ${username} = {
+            id = 0;
+            name = "${username}";
+            search = sharedSearch;
+            settings = sharedSettings;
+          };
+
+          test = {
+            id = 1;
+            name = "test";
+            search = sharedSearch;
+            settings = {
               "browser.bookmarks.addedImportButton" = false;
               "browser.bookmarks.restore_default_bookmarks" = false;
               "browser.formfill.enable" = true;
-              "browser.newtabpage.activity-stream.floorp.background.type" = 0;
               "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
               "browser.search.separatePrivateDefault" = false;
               "browser.startup.page" = 1;
-              "browser.uiCustomization.state" = "{\"placements\":{\"widget-overflow-fixed-list\":[\"profile-manager\"],\"unified-extensions-area\":[\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"home-button\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"statusBar\":[\"screenshot-button\",\"fullscreen-button\",\"status-text\"]},\"seen\":[\"developer-button\",\"sidebar-reverse-position-toolbar\",\"undo-closed-tab\",\"profile-manager\",\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"statusBar\",\"unified-extensions-area\",\"PersonalToolbar\",\"TabsToolbar\",\"toolbar-menubar\",\"widget-overflow-fixed-list\"],\"currentVersion\":20,\"newElementCount\":3}";
+              "browser.uiCustomization.state" =
+                "{\"placements\":{\"widget-overflow-fixed-list\":[\"profile-manager\"],\"unified-extensions-area\":[\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"home-button\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"],\"statusBar\":[\"screenshot-button\",\"fullscreen-button\",\"status-text\"]},\"seen\":[\"developer-button\",\"sidebar-reverse-position-toolbar\",\"undo-closed-tab\",\"profile-manager\",\"_0d7cafdd-501c-49ca-8ebb-e3341caaa55e_-browser-action\",\"_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_029d76e8-fbc9-4521-996a-014394515f7f_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"statusBar\",\"unified-extensions-area\",\"PersonalToolbar\",\"TabsToolbar\",\"toolbar-menubar\",\"widget-overflow-fixed-list\"],\"currentVersion\":20,\"newElementCount\":3}";
               "browser.urlbar.placeholderName.private" = "Startpage";
               "browser.urlbar.placeholderName" = "Startpage";
-              #"browser.urlbar.recentsearches.lastDefaultChanged" = "1743041709336";
               "browser.urlbar.shortcuts.bookmarks" = false;
               "browser.urlbar.shortcuts.history" = false;
               "browser.urlbar.shortcuts.tabs" = false;
@@ -75,64 +136,15 @@
               "privacy.clearOnShutdown_v2.cookiesAndStorage" = true;
               "privacy.donottrackheader.enabled" = true;
               "privacy.globalprivacycontrol.enabled" = true;
-              "privacy.sanitize.pending" = "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"siteSettings\",\"historyFormDataAndDownloads\",\"cookiesAndStorage\"],\"options\":{}}]";
+              "privacy.sanitize.pending" =
+                "[{\"id\":\"shutdown\",\"itemsToClear\":[\"cache\",\"siteSettings\",\"historyFormDataAndDownloads\",\"cookiesAndStorage\"],\"options\":{}}]";
               "services.sync.prefs.sync.floorp.browser.note.memos" = false;
               "signon.rememberSignons" = false;
               "userChrome.autohide.back_button" = true;
               "userChrome.autohide.forward_button" = true;
               "webgl.disabled" = true;
+              "browser.privatebrowsing.autostart" = true;
             };
-        in
-        {
-          default = {
-            id = 0;
-            name = "default";
-
-            search = {
-              force = true;
-              default = "Startpage"; # DuckDuckGo
-              privateDefault = "Startpage"; # DuckDuckGo
-            };
-
-            bookmarks = {
-              force = true;
-              settings = [
-                {
-                  name = "Nix sites";
-                  toolbar = true;
-                  bookmarks = [
-                    {
-                      name = "homepage";
-                      url = "https://nixos.org/";
-                    }
-                    {
-                      name = "wiki";
-                      tags = [
-                        "wiki"
-                        "nix"
-                      ];
-                      url = "https://wiki.nixos.org/";
-                    }
-                  ];
-                }
-              ];
-            };
-
-            settings = sharedSettings;
-          };
-
-          ${username} = {
-            id = 1;
-            name = "${username}";
-
-            settings = sharedSettings;
-          };
-
-          test = {
-            id = 2;
-            name = "test";
-
-            settings = sharedSettings;
           };
         };
     };
