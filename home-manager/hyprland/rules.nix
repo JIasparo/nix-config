1deae433d9837a67c529e99969f92be1eaed4fa6
j <j@test.email>
2025-03-28 17:45:24 -0400

    added simple workspace rules

--- home-manager/hyprland/rules.nix
+++ home-manager/hyprland/rules.nix
@@ -1,7 +1,28 @@
-{...}:
+{ ... }:
 
 {
   config = {
-    #
+    wayland.windowManager.hyprland.settings = {
+      workspace = [
+        "1, monitor:DP-2, default:true"
+        "2, monitor:DP-2"
+        "3, monitor:DP-2"
+        "4, monitor:DP-2"
+        "5, monitor:DP-2"
+        "6, monitor:DP-2"
+        "7, monitor:DP-2"
+        "8, monitor:DP-1, default:true"
+        "9, monitor:DP-1"
+        "10, monitor:DP-1"
+      ];
+
+      windowrule = [
+        #
+      ];
+
+      layerrule = [
+        #
+      ];
+    };
   };
 }
