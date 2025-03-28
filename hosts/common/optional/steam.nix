b5fe23cc523a9991b309d258427735d6f03cd689
j <j@test.email>
2025-03-27 20:09:18 -0400

    added args to gamescope

--- hosts/common/optional/steam.nix
+++ hosts/common/optional/steam.nix
@@ -5,19 +5,29 @@
     # Enable drivers for Xbox One controllers.
     #hardware.xone.enable = true;
 
-    programs.steam = {
-      enable = true;
+    programs = {
+      steam = {
+        enable = true;
 
-      # Opens firewall ports to allow Steam Remote Play services.
-      remotePlay.openFirewall = true;
+        # Opens firewall ports to allow Steam Remote Play services.
+        remotePlay.openFirewall = true;
 
-      #gamescopeSession.enable = true;
-      
-      # These packages are added to the Steam compatibility tools.
-      extraCompatPackages = with pkgs; [
-        # Glorious Eggroll Proton fork.
-        proton-ge-bin
-      ];
+        gamescopeSession.enable = true;
+
+        # These packages are added to the Steam compatibility tools.
+        extraCompatPackages = with pkgs; [
+          # Glorious Eggroll Proton fork.
+          proton-ge-bin
+        ];
+      };
+
+      gamescope = {
+        enable = true;
+        capSysNice = true;
+        args = [
+          "--expose-wayland"
+        ];
+      };
     };
   };
 }
