f5054c55c3ca360814e11f8af7a3cfb5f19196c0
j <j@test.email>
2025-03-27 23:40:49 -0400

    adjusted hyprland settings

--- home-manager/hyprland/settings.nix
+++ home-manager/hyprland/settings.nix
@@ -11,32 +11,63 @@
         "XDG_SESSION_DESKTOP,Hyprland"
       ];
 
+      #exec-once = [];
+
       general = {
-        gaps_in = "5";
-        gaps_out = "20";
-
+        gaps_in = "3";
+        gaps_out = "9";
         border_size = "2";
-
-        # Set to true enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false;
 
         # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
         allow_tearing = false;
+        resize_on_border = false;
 
         layout = "dwindle";
       };
 
+      dwindle = {
+        pseudotile = true;
+        preserve_split = false;
+      };
+
+      decoration = {
+        rounding = 10;
+        rounding_power = 3.0;
+
+        blur = {
+          enabled = true;
+          brightness = 1.0;
+          contrast = 1.0;
+          noise = 0.01;
+
+          size = 5;
+          passes = 3;
+        };
+
+        shadow = {
+          enabled = true;
+          offset = "0 15";
+          range = 4;
+          render_power = 2;
+          scale = 1.0;
+        };
+      };
+
+      animations = {
+        enabled = true;
+        
+        animation = [
+          "border, 1, 2, default"
+          "fade, 1, 4, default"
+          "windows, 1, 3, default, popin 80%"
+          "workspaces, 1, 2, default, slide"
+        ];
+      };
+
       input = {
         kb_layout = "us";
-        kb_variant = "";
-        kb_model = "";
-        kb_options = "";
-        kb_rules = "";
-
         follow_mouse = "1";
-
         sensitivity = "0"; # -1.0 - 1.0, 0 means no modification.
-
         numlock_by_default = true;
 
         touchpad = {
@@ -46,6 +77,15 @@
 
       gestures = {
         workspace_swipe = false;
+      };
+
+      misc = {
+        disable_autoreload = true;
+        vrr = 1;
+      };
+
+      render = {
+        direct_scanout = 2; # 0 - off, 1 - on, 2 - on for games
       };
     };
   };
