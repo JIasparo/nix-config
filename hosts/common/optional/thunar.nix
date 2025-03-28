612481c3446190144193263a3fba428e43ceb097
j <j@test.email>
2025-03-27 20:09:09 -0400

    added thunar module

---
+++ hosts/common/optional/thunar.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  config = {
+    programs.thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-media-tags-plugin
+        thunar-volman
+      ];
+    };
+
+    services = {
+      gvfs.enable = true;
+      tumbler.enable = true;
+    };
+  };
+}
