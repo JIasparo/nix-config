0cada019524a3414e95e770b23cb1b86b80ed68f
j <j@test.email>
2025-03-28 17:43:56 -0400

    renamed workspace 0 to workspace 10

--- home-manager/hyprland/binds.nix
+++ home-manager/hyprland/binds.nix
@@ -70,7 +70,6 @@
         "$mod CTRL, $down, splitratio, 0.1"
 
         # Switch workspaces with $mod + [0-9]
-        "$mod, 0, workspace, 0"
         "$mod, 1, workspace, 1"
         "$mod, 2, workspace, 2"
         "$mod, 3, workspace, 3"
@@ -80,9 +79,9 @@
         "$mod, 7, workspace, 7"
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
 
         # Move active window to a workspace with $mod + $move + [0-9]
-        "$mod $move, 0, movetoworkspace, 0"
         "$mod $move, 1, movetoworkspace, 1"
         "$mod $move, 2, movetoworkspace, 2"
         "$mod $move, 3, movetoworkspace, 3"
@@ -92,6 +91,7 @@
         "$mod $move, 7, movetoworkspace, 7"
         "$mod $move, 8, movetoworkspace, 8"
         "$mod $move, 9, movetoworkspace, 9"
+        "$mod $move, 0, movetoworkspace, 10"
       ];
 
       bindm = [
