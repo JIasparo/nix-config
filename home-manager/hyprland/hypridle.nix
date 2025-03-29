eb1930759ba7a5f28d9fb27fa89b9f38f2e83891
j <j@test.email>
2025-03-29 01:15:19 -0400

    configured hypridle

--- home-manager/hyprland/hypridle.nix
+++ home-manager/hyprland/hypridle.nix
@@ -4,9 +4,34 @@
   config = {
     services.hypridle = {
       enable = true;
-
       settings = {
-        #
+
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+        };
+
+        listener = [
+          {
+            # lock the screen after 10 minutes of inactivity
+            timeout = 600;
+            on-timeout = "loginctl lock-session";
+          }
+
+          {
+            # Turn off monitor(s) after 15 minutes of inactivity
+            timeout = 900;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+
+          {
+            # Suspend PC after 30 minutes of inactivity
+            timeout = 1800;
+            on-timeout = "systemctl suspend";
+          }
+        ];
       };
     };
   };
