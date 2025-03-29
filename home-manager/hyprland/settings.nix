18611d603c7fd5e705def4228cd7ebecf5299756
j <j@test.email>
2025-03-29 01:14:36 -0400

    changed environment variables

--- home-manager/hyprland/settings.nix
+++ home-manager/hyprland/settings.nix
@@ -4,7 +4,6 @@
   config = {
     wayland.windowManager.hyprland.settings = {
       env = [
-        "NIXOS_OZONE_WL,1"
         "XDG_CURRENT_DESKTOP,Hyprland"
         "XDG_SESSION_TYPE,wayland"
         "XDG_SESSION_DESKTOP,Hyprland"
