4033254e121b428319ad7e19dec4ad0e155c330d
j <j@test.email>
2025-03-30 02:39:16 -0400

    changed some settings

--- home-manager/yazi/settings.nix
+++ home-manager/yazi/settings.nix
@@ -3,7 +3,18 @@
 {
   config = {
     programs.yazi.settings = {
-      #
+      manager = {};
+
+      preview = {
+        max_width = 1200;
+        max_height = 1800;
+      };
+
+      opener = {};
+
+      open = {};
+
+      tasks = {};
     };
   };
 }
