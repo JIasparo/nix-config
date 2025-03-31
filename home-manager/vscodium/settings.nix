bd24ef92c51432131ea0a6828421890c62dd8141
j <j@test.email>
2025-03-31 16:53:05 -0400

    added user settings for the default profile

--- home-manager/vscodium/settings.nix
+++ home-manager/vscodium/settings.nix
@@ -7,6 +7,9 @@
         userSettings = {
           "telemetry.telemetryLevel" = "off";
           "editor.selectionClipboard" = false; # Turns off middle mouse paste
+          "workbench.activityBar.location" = "top";
+          "breadcrumbs.enabled" = false;
+          "workbench.tree.indent" = 15;
         };
       };
     };
