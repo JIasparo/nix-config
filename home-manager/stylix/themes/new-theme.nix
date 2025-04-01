f7d37b38f89c20c2c511495c7e9546fe81614470
j <j@test.email>
2025-04-01 18:06:19 -0400

    created a new theme

---
+++ home-manager/stylix/themes/new-theme.nix
@@ -0,0 +1,71 @@
+{ ... }:
+
+{
+  config = {
+    stylix = {
+      polarity = "dark"; # choose one: "dark" "light" "either"
+
+      image = ./../../../wallpapers/pexels-serinus-5602124.png;
+
+      base16Scheme = {
+        base00 = "141414"; # Default Background
+        base01 = "1f1f1f"; # Lighter Background (Used for status bars, line number and folding marks)
+        base02 = "313131"; # Selection Background
+        base03 = "4c4c4c"; # Comments, Invisibles, Line Highlighting
+        base04 = "777777"; # Dark Foreground (Used for status bars)
+        base05 = "959595"; # Default Foreground, Caret, Delimiters, Operators
+        base06 = "bababa"; # Light Foreground (Not often used)
+        base07 = "e9e9e9"; # Light Background (Not often used)
+        base08 = "b32834"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+        base09 = "c5693e"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
+        base0A = "d7aa48"; # Classes, Markup Bold, Search Text Background
+        base0B = "aba65d"; # Strings, Inherited Class, Markup Code, Diff Inserted
+        base0C = "8ea27b"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+        base0D = "709d99"; # Functions, Methods, Attribute IDs, Headings, Accent color
+        base0E = "bd843b"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+        base0F = "597479"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+      };
+
+      /*
+        iconTheme = {
+          enable = true;
+          package = pkgs.test;
+          light = "";
+          dark = "";
+        };
+      */
+
+      /*
+        fonts = {
+          serif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Serif";
+          };
+
+          sansSerif = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Sans";
+          };
+
+          monospace = {
+            package = pkgs.dejavu_fonts;
+            name = "DejaVu Sans Mono";
+          };
+
+          emoji = {
+            package = pkgs.noto-fonts-emoji;
+            name = "Noto Color Emoji";
+          };
+        };
+      */
+
+      /*
+        cursor = {
+          name = "";
+          package = pkgs.test;
+          size = 12;
+        };
+      */
+    };
+  };
+}
