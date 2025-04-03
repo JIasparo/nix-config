40088a685663d04c67009f97818c1388ad2ce65e
j <j@test.email>
2025-04-03 00:56:18 -0400

    commented out the env variable to force apps to use Wayland. Electron apps are way too glitchy for me, so I will try running them on xWayland.

--- hosts/common/optional/hyprland/default.nix
+++ hosts/common/optional/hyprland/default.nix
@@ -10,6 +10,10 @@
     programs.hyprland.enable = true;
 
     # Force apps to use Wayland
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    #environment.sessionVariables = {
+    #  NIXOS_OZONE_WL = "1";
+    #  ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    #};
+
   };
 }
