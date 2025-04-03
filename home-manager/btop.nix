98fe4b5b5f478124eef5768c1c0199dd884bfbc2
j <j@test.email>
2025-04-03 17:44:17 -0400

    added settings and changed the btop package to show Nvidia GPUs

--- home-manager/btop.nix
+++ home-manager/btop.nix
@@ -1,9 +1,14 @@
-{...}:
+{ pkgs, ... }:
 
 {
   config = {
     programs.btop = {
       enable = true;
+      package = pkgs.btop-cuda; # btop-cuda for Nvidia GPUs | btop-rocm for AMD GPUs
+
+      settings = {
+        shown_boxes = "proc cpu mem net gpu0";
+      };
     };
   };
 }
