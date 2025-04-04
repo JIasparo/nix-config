aaced05e5180ded80a321f846e66b4937e627b63
j <j@test.email>
2025-04-04 02:08:32 -0400

    enabled some settings

--- home-manager/nixcord.nix
+++ home-manager/nixcord.nix
@@ -1,13 +1,19 @@
 { ... }:
 
 {
-  imports = [
-    #inputs.nixcord.homeManagerModules.nixcord
-  ];
-
   config = {
     programs.nixcord = {
       enable = true;
+
+      discord = {
+        vencord.unstable = true;
+
+        autoscroll.enable = true;
+        settings = {
+          "OPEN_ON_STARTUP" = false;
+          "MINIMIZE_TO_TRAY" = false;
+        };
+      };
     };
   };
 }
