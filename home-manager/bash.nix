a0630dea7e086869c9cd1d2e1e9b85d04f10a262
j <j@test.email>
2025-04-17 00:42:55 -0400

    created vpn aliases

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -6,12 +6,18 @@
       enable = true;
 
       shellAliases = {
+        # NixOS and Home Manager aliases
         nf = "nix flake update --flake ./nix-config";
         hm = "home-manager switch --flake ./nix-config";
         no = "sudo nixos-rebuild switch --flake ./nix-config";
 
+        # General aliases
         ll = "ls -l";
         qqq = "poweroff";
+
+        # VPN aliases
+        vpn-start = "sudo systemctl start wg-quick-wg0.service";
+        vpn-stop = "sudo systemctl stop wg-quick-wg0.service";
       };
     };
   };
