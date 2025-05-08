48b8e2ea08c710e62aa9058f64bbefaab4e70050
j <j@test.email>
2025-05-07 23:57:23 -0400

    Attempt to declare OBS settings

--- home-manager/obs-studio/default.nix
+++ home-manager/obs-studio/default.nix
@@ -9,10 +9,11 @@
         #
       ];
     };
-  };
 
-  home.file = {
-    #".config/obs-studio/global.ini".source = ./global.ini;
-    #".config/obs-studio/user.ini".source = ./user.ini;
+    #home.file = {
+    #  ".config/obs-studio/global.ini".source = ./global.ini;
+    #  ".config/obs-studio/user.ini".source = ./user.ini;
+    #  ".config/obs-studio/basic/profiles/Untitled/basic.ini".source = ./basic.ini;
+    #};
   };
 }
