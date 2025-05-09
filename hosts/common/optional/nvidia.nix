043cf2952629e39a485748ebfd3de7e5e5a25e89
j <j@test.email>
2025-05-09 03:01:12 -0400

    removed 32bit graphics and reformatted the file

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -4,22 +4,29 @@
   config = {
     hardware.graphics = {
       enable = true;
-      enable32Bit = true;
     };
+
+    nixpkgs.config.cudaSupport = true;
 
     # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
+      # Modesetting is required
       modesetting.enable = true;
+
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       powerManagement.enable = true;
+
       # Experimental option to turn off GPU when not in use. Set to true if wanted.
       powerManagement.finegrained = false;
+
       # Use Nvidia open source kernel module. Set to true if wanted.
       open = true;
+
       # Enables the `nvidia-settings` menu. Set to true if wanted.
       nvidiaSettings = false;
+
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
