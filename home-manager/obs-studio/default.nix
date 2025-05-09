9614cc690a8c6254a684249f08c7dd56596ae12e
j <j@test.email>
2025-05-09 02:57:50 -0400

    added obs plugins, NVENC support, and more symlink files

--- home-manager/obs-studio/default.nix
+++ home-manager/obs-studio/default.nix
@@ -5,8 +5,11 @@
     programs.obs-studio = {
       enable = true;
 
+      package = (pkgs.obs-studio.override { cudaSupport = true; });
+
       plugins = with pkgs.obs-studio-plugins; [
-        #
+        obs-pipewire-audio-capture
+        obs-vkcapture
       ];
     };
 
@@ -14,6 +17,8 @@
     #  ".config/obs-studio/global.ini".source = ./global.ini;
     #  ".config/obs-studio/user.ini".source = ./user.ini;
     #  ".config/obs-studio/basic/profiles/Untitled/basic.ini".source = ./basic.ini;
+    #  ".config/obs-studio/basic/profiles/Untitled/recordEncoder.json".source = ./recordEncoder.json;
+    #  ".config/obs-studio/basic/profiles/Untitled/streamEncoder.json".source = ./streamEncoder.json;
     #};
   };
 }
