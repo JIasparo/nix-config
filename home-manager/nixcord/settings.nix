60d6a51eae6d73a56a5427c22cb29e895c686a5d
j <j@test.email>
2025-05-25 15:31:11 -0400

    removed reference to `settings.json`

--- home-manager/nixcord/settings.nix
+++ home-manager/nixcord/settings.nix
@@ -2,11 +2,19 @@
 
 {
   config = {
-    home.file = {
-      ".config/discord/settings.json".source = ./settings.json;
-    };
-
     programs.nixcord = {
+      discord = {
+        settings = {
+          "MINIMIZE_TO_TRAY" = false;
+          "OPEN_ON_STARTUP" = false;
+          "SKIP_HOST_UPDATE" = true;
+          "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING" = true;
+          "openasar" = {
+            "setup" = true;
+          };
+        };
+      };
+
       config = {
         plugins = {
           accountPanelServerProfile = {
