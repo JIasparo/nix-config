7466f1302269a7fbbe18a7291bcf5bc63c925bbc
j <j@test.email>
2025-05-31 13:58:23 -0400

    moved vencord plugins from `settings.nix` to newly created `plugins.nix`

---
+++ home-manager/nixcord/plugins.nix
@@ -0,0 +1,115 @@
+{ ... }:
+
+{
+  config = {
+    programs.nixcord.config.plugins = {
+      accountPanelServerProfile = {
+        enable = true;
+        prioritizeServerProfile = true;
+      };
+
+      alwaysExpandRoles.enable = true;
+
+      betterFolders = {
+        enable = true;
+        sidebar = true;
+        sidebarAnim = false;
+        closeAllFolders = true;
+        closeAllHomeButton = false;
+        closeOthers = true;
+        forceOpen = false;
+        keepIcons = false;
+        showFolderIcon = "never";
+      };
+
+      clearURLs.enable = true;
+
+      dontRoundMyTimestamps.enable = true;
+
+      fakeNitro = {
+        enable = true;
+        enableEmojiBypass = true;
+        emojiSize = 48;
+        transformEmojis = true;
+        enableStickerBypass = true;
+        stickerSize = 160;
+        transformStickers = true;
+        transformCompoundSentence = false;
+        enableStreamQualityBypass = true;
+        useHyperLinks = true;
+        hyperLinkText = "{{NAME}}";
+        disableEmbedPermissionCheck = false;
+      };
+
+      fakeProfileThemes = {
+        enable = true;
+        nitroFirst = true;
+      };
+
+      fixYoutubeEmbeds.enable = true;
+
+      forceOwnerCrown.enable = true;
+
+      newGuildSettings = {
+        enable = true;
+        guild = true;
+        messages = "serverDefault";
+        everyone = false;
+        role = false;
+        highlights = true;
+        events = true;
+        showAllChannels = true;
+      };
+
+      previewMessage.enable = true;
+
+      replyTimestamp.enable = true;
+
+      roleColorEverywhere = {
+        enable = true;
+        chatMentions = true;
+        memberList = true;
+        voiceUsers = true;
+        reactorsList = true;
+        colorChatMessages = true;
+        messageSaturation = 30;
+      };
+
+      showHiddenChannels = {
+        enable = true;
+        hideUnreads = true;
+        showMode = "muted";
+        showHiddenChannels = true;
+      };
+
+      typingIndicator = {
+        enable = true;
+        includeCurrentChannel = true;
+        includeMutedChannels = true;
+        includeBlockedUsers = true;
+        indicatorMode = "both";
+      };
+
+      typingTweaks = {
+        enable = true;
+        showAvatars = true;
+        showRoleColors = true;
+        alternativeFormatting = true;
+      };
+
+      userMessagesPronouns = {
+        enable = true;
+        pronounsFormat = "LOWERCASE";
+        showSelf = true;
+      };
+
+      voiceMessages = {
+        enable = true;
+        noiseSuppression = true;
+        echoCancellation = true;
+      };
+
+      youtubeAdblock.enable = true;
+    };
+  };
+}
