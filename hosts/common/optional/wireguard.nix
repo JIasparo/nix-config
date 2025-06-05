08f03091d36d83fb1e0bebc524edb20686cf1bd6
j <j@test.email>
2025-06-05 00:02:59 -0400

    Added comments for specific Proton VPN servers

--- hosts/common/optional/wireguard.nix
+++ hosts/common/optional/wireguard.nix
@@ -4,6 +4,11 @@
   config = {
     networking.wg-quick.interfaces = {
       wg0 = {
+        /*
+          Proton VPN:
+          US 79 = San Jose, California
+          US 111 = Los Angeles, California
+        */
         autostart = false;
         configFile = "/root/wireguard/wg0.conf";
       };
