3700958d93a892fe574a1b8fd0e004a173713789
j <j@test.email>
2025-06-06 14:31:04 -0400

    added plugins directory to yazi

--- home-manager/yazi/default.nix
+++ home-manager/yazi/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./binds.nix
     ./settings.nix
+    ./plugins
   ];
 
   config = {
---
+++ home-manager/yazi/plugins/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  imports = [
+    ./mediainfo.nix
+    #./starship.nix
+  ];
+}
