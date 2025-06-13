55235e159f94aebce06011fa37f704491168d23c
j <j@test.email>
2025-06-12 22:48:56 -0400

    created some aliases to make setting up new hosts easier

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, hostname, ... }:
 
 {
   config = {
@@ -12,6 +12,11 @@
         hn = "home-manager news";
         no = "sudo nixos-rebuild switch --flake ./nix-config";
         full-upgrade = "${nf} && ${no} && ${hm}";
+
+        #disko-format = "sudo nix --experimental-features \"nix-command flakes\" run github:nix-community/disko -- --flake ./nix-config#${hostname} --mode destroy,format,mount ./nix-config/hosts/nixos/${hostname}/disk-configuration.nix";
+        #nix-install = "sudo nixos-install --flake ./nix-config#${hostname}";
+        #home-standalone-install = "nix run home-manager/master -- init --switch ./nix-config";
+        #full-install = "${disko-format} && ${nix-install} && ${home-standalone-install}";
 
         # General aliases
         ff = "${pkgs.fastfetch}/bin/fastfetch";
