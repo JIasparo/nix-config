65ab7594e8d47e8f8412c3095299ab802a06cd13
j <j@test.email>
2025-06-16 15:21:18 -0400

    created a laptop module

---
+++ hosts/common/optional/laptop.nix
@@ -0,0 +1,26 @@
+{ ... }:
+
+{
+  config = {
+    services = {
+      # Prevents overheating on Intel CPUs.
+      thermald.enable = true;
+
+      auto-cpufreq = {
+        enable = true;
+
+        settings = {
+          battery = {
+            governor = "powersave";
+            turbo = "never";
+          };
+          
+          charger = {
+            governor = "performance";
+            turbo = "auto";
+          };
+        };
+      };
+    };
+  };
+}
