c4eb72a75f6632490b00ca2b353dd510729e4f64
j <j@test.email>
2025-06-21 01:10:53 -0400

    manage Discord dictionary with `home-manager`

---
+++ home-manager/nixcord/dictionary.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  config = {
+    home.file = {
+      ".config/discord/Custom Dictionary.txt".text = ''
+        depersonalization
+        derealization
+      '';
+    };
+  };
+}
