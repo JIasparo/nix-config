056bf99ecffc1addb653d3c99fa213664b15161b
j <j@test.email>
2025-06-29 00:19:19 -0400

    added a bind for rofi

--- home-manager/niri/binds.nix
+++ home-manager/niri/binds.nix
@@ -4,6 +4,7 @@
   config = {
     programs.niri.settings.binds = with config.lib.niri.actions; {
       "Mod+Return".action = spawn "kitty";
+      "Mod+Space".action = spawn "rofi -show drun";
       "Mod+E".action = spawn "codium";
       "Mod+F".action = spawn "kitty yazi";
       "Mod+W".action = spawn "librewolf";
