c02568124ab3cccc02ecd1ec362349759afc693e
j <j@test.email>
2025-08-16 15:08:59 -0400

    reformatted and added imports

--- hosts/nixos/stryder/default.nix
+++ hosts/nixos/stryder/default.nix
@@ -6,11 +6,14 @@
     ./disk-configuration.nix
     ./hardware-configuration.nix
     ./../../common/core
+    
     # Optional
     #./../../common/optional/gnome.nix
     ./../../common/optional/hyprland
     #./../../common/optional/niri
     ./../../common/optional/nvidia.nix
+    ./../../common/optional/openrgb.nix
+    ./../../common/optional/ratbagd.nix
     ./../../common/optional/steam.nix
     ./../../common/optional/wireguard.nix
   ];
