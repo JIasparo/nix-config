fd73f510bcb8bcf40e3428f316200b7e9132a519
j <j@test.email>
2025-08-26 16:31:32 -0400

    changed filters for blue-yeti microphone

--- home-manager/easyeffects/presets/blue-yeti.nix
+++ home-manager/easyeffects/presets/blue-yeti.nix
@@ -6,18 +6,43 @@
       blue-yeti = {
         input = {
           blocklist = [ ];
+
           "plugins_order" = [
             "rnnoise#0"
+            "speex#0"
           ];
+
           "rnnoise#0" = {
             bypass = false;
-            "enable-vad" = false;
-            "input-gain" = 0.0;
-            "model-path" = "";
-            "output-gain" = 0.0;
-            release = 20.0;
+            "model-name" = "";
+
+            "enable-vad" = true;
             "vad-thres" = 50.0;
             wet = 0.0;
+            release = 20.0;
+
+            "input-gain" = 0.0;
+            "output-gain" = 0.0;
+          };
+
+          "speex#0" = {
+            bypass = false;
+
+            "enable-denoise" = true;
+            "noise-suppression" = -70;
+
+            "enable-agc" = false;
+            "enable-dereverb" = false;
+
+            "vad" = {
+              enable = true;
+
+              "probability-start" = 95;
+              "probability-continue" = 90;
+            };
+
+            "input-gain" = 0.0;
+            "output-gain" = 0.0;
           };
         };
       };
