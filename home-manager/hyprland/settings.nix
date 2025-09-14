0ba39ab4811a15c28c1710d2f01c7859c2153bbc
j <j@test.email>
2025-09-14 11:10:28 -0400

    removed `gestures:workspace_swipe`

--- home-manager/hyprland/settings.nix
+++ home-manager/hyprland/settings.nix
@@ -85,10 +85,6 @@
         };
       };
 
-      gestures = {
-        workspace_swipe = false;
-      };
-
       misc = {
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
