cfaaa39d7899cd9f0abe8afba94702201b5f6b33
j <j@test.email>
2025-09-21 17:50:43 -0400

    created `thunar.nix`

---
+++ hosts/common/optional/thunar.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  config = {
+    programs.thunar = {
+      enable = true;
+      
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-media-tags-plugin
+        thunar-volman
+      ];
+    };
+  };
+}
