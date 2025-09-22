0e2ef6c97a41c996848b98465cfa1709a791a45d
j <j@test.email>
2025-09-22 02:01:32 -0400

    added additional config to fix more audio issues

--- hosts/common/core/pipewire.nix
+++ hosts/common/core/pipewire.nix
@@ -49,6 +49,23 @@
                 }
               ];
             };
+
+            "alsa-config" = {
+              "monitor.alsa.rules" = [
+                {
+                  matches = [
+                    { "node.name" = "~alsa_output.*"; }
+                  ];
+
+                  actions = {
+                    update-props = {
+                      "api.alsa.period-size" = 1024;
+                      "api.alsa.headroom" = 8192;
+                    };
+                  };
+                }
+              ];
+            };
           };
         };
       };
