9dd4333fb0e0b39c20af4de84be8b75932ffeed0
j <j@test.email>
2025-09-22 02:01:45 -0400

    removed unused plugins

--- hosts/common/optional/thunar.nix
+++ hosts/common/optional/thunar.nix
@@ -4,10 +4,8 @@
   config = {
     programs.thunar = {
       enable = true;
-      
+
       plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-media-tags-plugin
         thunar-volman
       ];
     };
