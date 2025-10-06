8c7c916bf9ed5d3757e28a47db554d4ccae6a463
j <j@test.email>
2025-10-05 21:28:17 -0400

    imported pipewire configurations

--- hosts/nixos/stryder/default.nix
+++ hosts/nixos/stryder/default.nix
@@ -13,6 +13,8 @@
     #./../../common/optional/niri
     ./../../common/optional/nvidia.nix
     ./../../common/optional/openrgb.nix
+    ./../../common/optional/pipewire-fixes/disable-suspension.nix
+    #./../../common/optional/pipewire-fixes/increase-headroom.nix
     ./../../common/optional/ratbagd.nix
     ./../../common/optional/steam.nix
     ./../../common/optional/thunar.nix
