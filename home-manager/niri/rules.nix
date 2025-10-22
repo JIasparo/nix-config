17b8c5f0444371b20a1d982abb6cf6b578f6f871
j <j@test.email>
2025-10-21 20:35:22 -0400

    created workspaces for niri

--- home-manager/niri/rules.nix
+++ home-manager/niri/rules.nix
@@ -4,27 +4,37 @@
   config = {
     programs.niri.settings = {
       spawn-at-startup = [
-        { command = [ "bitwarden" ]; }
+        #{ argv = [ "bitwarden" ]; }
       ];
 
       workspaces = {
-        "01" = {
+        "workspace-1" = {
           name = "1";
           open-on-output = "DP-2";
         };
 
-        "02" = {
+        "workspace-2" = {
           name = "2";
           open-on-output = "DP-2";
         };
 
-        "03" = {
+        "workspace-3" = {
           name = "3";
+          open-on-output = "DP-2";
+        };
+
+        "workspace-4" = {
+          name = "4";
           open-on-output = "DP-1";
         };
 
-        "04" = {
-          name = "4";
+        "workspace-5" = {
+          name = "5";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-6" = {
+          name = "6";
           open-on-output = "DP-1";
         };
       };
