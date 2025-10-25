f74473dcd0be8310303da8d7d42f3f511dec4dd1
j <j@test.email>
2025-10-24 20:01:37 -0400

    added words to Discord dictionary

--- home-manager/nixcord/dictionary.nix
+++ home-manager/nixcord/dictionary.nix
@@ -4,8 +4,10 @@
   config = {
     home.file = {
       ".config/discord/Custom Dictionary.txt".text = ''
+        Chornobyl
         depersonalization
         derealization
+        Prypiat
       '';
     };
   };
