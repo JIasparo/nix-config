0157fe8e95582ece32015bb3741b4c03ff7d11da
j <j@test.email>
2025-10-31 00:53:10 -0400

    commented out niri from `inputs` and `outputs`

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    niri.url = "github:sodiboo/niri-flake";
-    niri.inputs.nixpkgs.follows = "nixpkgs";
+    #niri.url = "github:sodiboo/niri-flake";
+    #niri.inputs.nixpkgs.follows = "nixpkgs";
 
     nixcord.url = "github:kaylorben/nixcord";
     nixcord.inputs.nixpkgs.follows = "nixpkgs";
@@ -42,7 +42,7 @@
           modules = [
             ./hosts/nixos/${hostname}
             inputs.disko.nixosModules.disko
-            inputs.niri.nixosModules.niri
+            #inputs.niri.nixosModules.niri
           ];
           specialArgs = { inherit inputs username useremail hostname configpath; };
         };
@@ -55,8 +55,8 @@
             ./home-manager
             inputs.stylix.homeModules.stylix
             inputs.nixcord.homeModules.nixcord
-            inputs.niri.homeModules.niri
-            inputs.niri.homeModules.stylix
+            #inputs.niri.homeModules.niri
+            #inputs.niri.homeModules.stylix
           ];
           extraSpecialArgs = { inherit inputs username useremail hostname configpath; };
         };
