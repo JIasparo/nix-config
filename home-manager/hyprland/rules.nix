9af2fda37481a9610f3e1e73c43c597dcff8b4d1
j <j@test.email>
2026-01-02 16:05:43 -0500

    updated all window rules to the new system (Hyprland 0.53.0)

--- home-manager/hyprland/rules.nix
+++ home-manager/hyprland/rules.nix
@@ -3,10 +3,6 @@
 {
   config = {
     wayland.windowManager.hyprland.settings = {
-      exec-once = [
-        "[workspace special silent] $passManager"
-      ];
-
       workspace = [
         "1, monitor:DP-2, default:true"
         "2, monitor:DP-2"
@@ -18,110 +14,111 @@
         "8, monitor:DP-1, default:true"
         "9, monitor:DP-1"
         "10, monitor:DP-1"
+        "special:special, on-created-empty:$passManager"
       ];
 
-      windowrulev2 = [
+      windowrule = [
         # Assign tags for windows
         # Browser tags
-        "tag +browser, class:^([Ll]ibrewolf)$"
-        "tag +browser, class:^([Gg]oogle-chrome(-beta|-dev|-unstable)?)$"
-        "tag +browser, class:^(chrome-.+-Default)$" # Chrome PWAs
-        "tag +browser, class:^([Cc]hromium)$"
-        "tag +browser, class:^(Brave-browser(-beta|-dev|-unstable)?)$"
-        "tag +browser, class:^([Tt]horium-browser|[Cc]achy-browser)$"
-        "tag +browser, class:^(zen-alpha|zen)$"
+        "tag +browser, match:class ^([Ll]ibrewolf)$"
+        "tag +browser, match:class ^([Gg]oogle-chrome(-beta|-dev|-unstable)?)$"
+        "tag +browser, match:class ^(chrome-.+-Default)$" # Chrome PWAs
+        "tag +browser, match:class ^([Cc]hromium)$"
+        "tag +browser, match:class ^(Brave-browser(-beta|-dev|-unstable)?)$"
+        "tag +browser, match:class ^([Tt]horium-browser|[Cc]achy-browser)$"
+        "tag +browser, match:class ^(zen-alpha|zen)$"
 
         # Notification tags
-        "tag +notif, class:^(swaync-control-center|swaync-notification-window|swaync-client|class)$"
+        "tag +notif, match:class ^(swaync-control-center|swaync-notification-window|swaync-client|class)$"
 
         # Terminal tags
-        "tag +terminal, class:^(Alacritty|kitty|kitty-dropterm)$"
+        "tag +terminal, match:class ^(Alacritty|kitty|kitty-dropterm)$"
 
         # Editor tags
-        "tag +editor, class:^(codium|codium-url-handler|VSCodium)$"
+        "tag +editor, match:class ^(codium|codium-url-handler|VSCodium)$"
 
         # Recording tags
-        "tag +recording, class:^(com.obsproject.Studio)$"
+        "tag +recording, match:class ^(com.obsproject.Studio)$"
 
         # Instant Messenger tags
-        "tag +im, class:^([Dd]iscord|[Ww]ebCord|[Vv]esktop)$"
+        "tag +im, match:class ^([Dd]iscord|[Ww]ebCord|[Vv]esktop)$"
 
         # Game tags
-        "tag +games, class:^(gamescope)$"
-        "tag +games, class:^(steam_app_\\d+)$"
-        "tag +games, title:^([Bb]attlefield 4)$"
-        "tag +games, title:^([Tt]itanfall 2)$"
-        "tag +games, class:^(Slay the Spire)$"
-        "tag +games, class:^(.*)(.x86_64)$"
-        "tag +games, initialTitle:^([Gg]odot)$"
+        "tag +games, match:class ^(gamescope)$"
+        "tag +games, match:class ^(steam_app_\\d+)$"
+        "tag +games, match:title ^([Bb]attlefield 4)$"
+        "tag +games, match:title ^([Tt]itanfall 2)$"
+        "tag +games, match:class ^(Slay the Spire)$"
+        "tag +games, match:class ^(.*)(.x86_64)$"
+        "tag +games, match:initial_title ^([Gg]odot)$"
 
         # Game launcher tags
-        "tag +game-launcher, class:^([Ss]team)$"
-        "tag +game-launcher, title:^([Ll]utris)$"
-        "tag +game-launcher, title:^(EA)$"
-        "tag +game-launcher, class:^([Hh]eroic)$"
-        "tag +game-launcher, class:^(org.prismlauncher.PrismLauncher)$"
+        "tag +game-launcher, match:class ^([Ss]team)$"
+        "tag +game-launcher, match:title ^([Ll]utris)$"
+        "tag +game-launcher, match:title ^(EA)$"
+        "tag +game-launcher, match:class ^([Hh]eroic)$"
+        "tag +game-launcher, match:class ^(org.prismlauncher.PrismLauncher)$"
 
         # File-manager tags
-        "tag +file-manager, class:^([Tt]hunar)$"
-        "tag +file-manager, title:^([Yy]azi|[Yy]azi.*)$"
+        "tag +file-manager, match:class ^([Tt]hunar)$"
+        "tag +file-manager, match:title ^([Yy]azi|[Yy]azi.*)$"
 
-        # Multimedia-video tags
-        "tag +multimedia-video, class:^([Mm]pv|vlc)$"
+        # Multimedia tags
+        "tag +multimedia, match:class ^([Mm]pv|vlc)$"
 
         # Sensitive tags
-        "tag +sensitive, class:^([Bb]itwarden)$"
+        "tag +sensitive, match:class ^([Bb]itwarden)$"
 
         # Pop-up tags
-        "tag +pop-up, title:^(Open File)(.*)$"
-        "tag +pop-up, title:^(Select a File)(.*)$"
-        "tag +pop-up, title:^(Open Folder)(.*)$"
-        "tag +pop-up, title:^(Save As)(.*)$"
-        "tag +pop-up, title:^(Save Workspace)(.*)$"
-        "tag +pop-up, title:^(Library)(.*)$"
-        "tag +pop-up, title:^(File Upload)(.*)$"
-        "tag +pop-up, title:^(.*)(wants to save)$"
-        "tag +pop-up, title:^(.*)(wants to open)$"
-        "tag +pop-up, title:^(Log out)(.*)$"
-        "tag +pop-up, title:^(Save Image As)(.*)$"
-        "tag +pop-up, title:^(File Operation Progress)$"
-        "tag +pop-up, title:^(Select Install Path)$"
+        "tag +pop-up, match:title ^(Open File)(.*)$"
+        "tag +pop-up, match:title ^(Select a File)(.*)$"
+        "tag +pop-up, match:title ^(Open Folder)(.*)$"
+        "tag +pop-up, match:title ^(Save As)(.*)$"
+        "tag +pop-up, match:title ^(Save Workspace)(.*)$"
+        "tag +pop-up, match:title ^(Library)(.*)$"
+        "tag +pop-up, match:title ^(File Upload)(.*)$"
+        "tag +pop-up, match:title ^(.*)(wants to save)$"
+        "tag +pop-up, match:title ^(.*)(wants to open)$"
+        "tag +pop-up, match:title ^(Log out)(.*)$"
+        "tag +pop-up, match:title ^(Save Image As)(.*)$"
+        "tag +pop-up, match:title ^(File Operation Progress)$"
+        "tag +pop-up, match:title ^(Select Install Path)$"
 
         # Assign window rules
         # Float, center, and resize pop-ups and dialogue boxes
-        "float, center, size 40% 40%, tag:pop-up*"
+        "match:tag pop-up*, float on, center on, size 40% 40%"
 
-        # Make multimedia-video windows opaque
-        "noblur, opaque, tag:multimedia-video*"
+        # Make multimedia windows opaque
+        "match:tag multimedia*, no_blur on, opaque on"
 
         # Make games float and center
-        "float, center, tag:games*"
+        "match:tag games*, float on, center on"
 
         # Disable eye candy for games
-        "decorate 0, tag:games*"
+        "match:tag games*, decorate off"
 
         # Prevent sensitive windows from showing in a screenshare
-        "noscreenshare, tag:sensitive*"
+        "match:tag sensitive*, no_screen_share on"
 
         # Prevent idling while playing games
-        "idleinhibit always, tag:games*"
+        "match:tag games*, idle_inhibit always"
 
         # Prevent idling while playing videos
-        "idleinhibit focus, tag:multimedia-video*"
-        "idleinhibit fullscreen, title:^(.*YouTube.*)$"
+        "match:tag multimedia*, idle_inhibit focus"
+        "match:title ^(.*YouTube.*)$, idle_inhibit fullscreen"
 
         # Workspace specific rules
         # Make game-launchers spawn on workspace 5
-        "workspace 5, tag:game-launcher*"
+        "match:tag game-launcher*, workspace 5"
 
         # Make games spawn on workspace 6
-        "workspace 6, tag:games*"
+        "match:tag games*, workspace 6"
 
         # Make instant messengers open on workspace 9
-        "workspace 9, tag:im*"
+        "match:tag im*, workspace 9"
 
         # Make recording software open on workspace 10
-        "workspace 10, tag:recording*"
+        "match:tag recording*, workspace 10"
       ];
 
       layerrule = [
