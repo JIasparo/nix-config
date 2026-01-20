8b5ecd0815732d5157dd5eb40b99485c41c6de73
j <j@test.email>
2026-01-20 18:17:44 -0500

    updated settings

--- home-manager/yazi/settings.nix
+++ home-manager/yazi/settings.nix
@@ -3,9 +3,12 @@
 {
   config = {
     programs.yazi.settings = {
-      manager = {};
+      mgr = {
+        ratio = [1 4 3];
+      };
 
       preview = {
+        wrap = "no";
         max_width = 1200;
         max_height = 1800;
       };
@@ -15,6 +18,10 @@
       open = {};
 
       tasks = {};
+      
+      input = {
+        cursor_blink = false;
+      };
     };
   };
 }
