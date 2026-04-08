22ebae06d3b679a1b8925c9abcfd398f53266fba
j <j@test.email>
2026-04-08 02:38:55 -0400

    added unfinished configuration

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -20,5 +20,18 @@
         ecosystem:no_update_news=true;
       '';
     };
+
+    # Taken from github:ryan4yin/nix-config.
+    # NOTE: this executable is used by greetd to start a wayland session when system boot up
+    # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+    #home.file.".wayland-session" = {
+    #  source = pkgs.writeScript "init-session" ''
+    #    # trying to stop a previous wayland compositor session
+    #    systemctl --user is-active hyprland.service && systemctl --user stop hyprland.service
+    #    # and then we start a new one
+    #    /run/current-system/sw/bin/Hyprland
+    #  '';
+    #  executable = true;
+    #};
   };
 }
