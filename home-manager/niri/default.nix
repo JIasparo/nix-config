734dbadf03d72b9f6ef416aa52784b2324d43a23
j <j@test.email>
2026-04-08 02:40:46 -0400

    added `ryan4yin`'s vendor-no-locking script

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -22,5 +22,18 @@
         path = "${lib.getExe pkgs.xwayland-satellite}";
       };
     };
+
+    # Taken from github:ryan4yin/nix-config.
+    # NOTE: this executable is used by greetd to start a wayland session when system boot up
+    # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+    home.file.".wayland-session" = {
+      source = pkgs.writeScript "init-session" ''
+        # trying to stop a previous wayland compositor session
+        systemctl --user is-active niri.service && systemctl --user stop niri.service
+        # and then we start a new one
+        /run/current-system/sw/bin/niri-session
+      '';
+      executable = true;
+    };
   };
 }
