a3cb494130e974399ed8451270ce16f0d733053b
j <j@test.email>
2026-04-08 02:41:43 -0400

    unfinished settings for yt-dlp

--- home-manager/yt-dlp/settings.nix
+++ home-manager/yt-dlp/settings.nix
@@ -1,9 +1,34 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   config = {
-    programs.yt-dlp.settings = {
-      #
+    programs.yt-dlp = {
+      extraConfig = ''
+        --alias get-m4a "-f 'ba[acodec^=opus]/ba/b' -x --audio-format m4a"
+      '';
+      #--alias get-m4a "-f 'ba[acodec^=opus]/ba[acodec^=aac]/ba[acodec^=mp4a.40.]/ba/b' -x --audio-format m4a"
+
+      settings = {
+        ffmpeg-location = "${lib.getExe pkgs.ffmpeg-full}";
+        paths = [
+          "Music/tag"
+        ];
+
+        output = [
+          "\"%(artist)s - %(title)s.%(ext)s\""
+        ];
+
+        embed-metadata = true;
+        #embed-thumbnail = true;
+
+        parse-metadata = [
+          #
+        ];
+
+        replace-in-metadata = [
+          #
+        ];
+      };
     };
   };
 }
