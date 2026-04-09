ede80bf5f8f62f3f0ffd8f94cc1001a1e4a8f100
j <j@test.email>
2026-04-09 02:53:33 -0400

    added tag rules

--- home-manager/mangowc/rules.nix
+++ home-manager/mangowc/rules.nix
@@ -4,12 +4,23 @@
   config = {
     wayland.windowManager.mango.settings = {
       tagrule = [
-        ""
-        ""
+        "id:1,layout_name:scroller"
+        "id:2,layout_name:scroller"
+        "id:3,layout_name:scroller"
+        "id:4,layout_name:scroller"
+        "id:5,layout_name:scroller"
+        "id:6,layout_name:scroller"
+        "id:7,layout_name:scroller"
+        "id:8,layout_name:scroller"
+        "id:9,layout_name:scroller"
       ];
 
       windowrule = [
-        "isnamedscratchpad:1,width:1280,height:800,title:password-manager"
+        #"isnamedscratchpad:1,width:1920,height:1080,title:password-manager"
+      ];
+
+      layerrule = [
+        #"noanim:1,layer_name:rofi"
       ];
     };
   };
