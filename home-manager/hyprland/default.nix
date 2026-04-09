f23c79d8eeab012b9cc739277839e37ab97d4110
j <j@test.email>
2026-04-09 02:55:38 -0400

    possibly fixed the vendor-no-locking script

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   imports = [
@@ -24,14 +24,14 @@
     # Taken from github:ryan4yin/nix-config.
     # NOTE: this executable is used by greetd to start a wayland session when system boot up
     # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
-    #home.file.".wayland-session" = {
-    #  source = pkgs.writeScript "init-session" ''
-    #    # trying to stop a previous wayland compositor session
-    #    systemctl --user is-active hyprland.service && systemctl --user stop hyprland.service
-    #    # and then we start a new one
-    #    /run/current-system/sw/bin/Hyprland
-    #  '';
-    #  executable = true;
-    #};
+    home.file.".wayland-session" = {
+      source = pkgs.writeScript "init-session" ''
+        # trying to stop a previous wayland compositor session
+        systemctl --user is-active Hyprland.service && systemctl --user stop Hyprland.service
+        # and then we start a new one
+        ${lib.getExe config.wayland.windowManager.hyprland.package}
+      '';
+      executable = true;
+    };
   };
 }
