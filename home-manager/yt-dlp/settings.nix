a8b4ca93c092a5e4a9f9bb29589edaedecfce13c
j <j@test.email>
2026-04-15 17:34:40 -0400

    removed unused settings and configs

--- home-manager/yt-dlp/settings.nix
+++ home-manager/yt-dlp/settings.nix
@@ -2,33 +2,27 @@
 
 {
   config = {
-    programs.yt-dlp = {
-      extraConfig = ''
-        --alias get-m4a "-f 'ba[acodec^=opus]/ba/b' -x --audio-format m4a"
-      '';
-      #--alias get-m4a "-f 'ba[acodec^=opus]/ba[acodec^=aac]/ba[acodec^=mp4a.40.]/ba/b' -x --audio-format m4a"
+    # yt-dlp -t aac --cookies ~/Downloads/cookies.txt
+    programs.yt-dlp.settings = {
+      ffmpeg-location = "${lib.getExe pkgs.ffmpeg-full}";
+      paths = [
+        "Music/tag"
+      ];
 
-      settings = {
-        ffmpeg-location = "${lib.getExe pkgs.ffmpeg-full}";
-        paths = [
-          "Music/tag"
-        ];
+      output = [
+        "\"%(artist)s - %(title)s.%(ext)s\""
+      ];
 
-        output = [
-          "\"%(artist)s - %(title)s.%(ext)s\""
-        ];
+      embed-metadata = true;
+      embed-thumbnail = true;
 
-        embed-metadata = true;
-        #embed-thumbnail = true;
+      parse-metadata = [
+        #
+      ];
 
-        parse-metadata = [
-          #
-        ];
-
-        replace-in-metadata = [
-          #
-        ];
-      };
+      replace-in-metadata = [
+        #
+      ];
     };
   };
 }
