fe0b5d420395207d6102ff697926068ba2fedf9e
j <j@test.email>
2026-05-04 00:32:59 -0400

    updated settings

--- home-manager/niri/settings.nix
+++ home-manager/niri/settings.nix
@@ -11,115 +11,127 @@
         { argv = [ "swaybg" "-i" "${config.stylix.image}" ]; }
       ];
 
+      hotkey-overlay = {
+        skip-at-startup = true; # Whether the hotkey overlay shows when niri starts.
+        hide-not-bound = false; # Niri shows important keybinds, even if they are not bound to a key. If enabled, niri will only show keybinds bound to a key.
+      };
+
+      screenshot-path = "~/Pictures/Screenshots/niri-%Y%m%d-%H:%M:%S.png"; # The path niri's built-in screenshot tool saves screenshots to.
+      clipboard.disable-primary = true; # Disable primary selection clipboard (middle mouse paste).
+      prefer-no-csd = true; # Whether to prefer server-side decorations (SSD) over client-side decorations (CSD).
+
+      # Input devices and related settings
+      input = {
+        keyboard = {
+          xkb.layout = "us";
+          numlock = true; # Enable NumLock on startup.
+        };
+
+        focus-follows-mouse = {
+          enable = true; # Whether focus follows the mouse.
+          max-scroll-amount = "0%"; # Focus-follows-mouse will not focus a window if it scrolls the screen more than this amount (0% - 100%).
+        };
+
+        mouse = {
+          accel-profile = "flat"; # What acceleration profile to use ("adaptive", "flat": disables pointer acceleration).
+          accel-speed = 0; # Pointer acceleration speed (-1.0 - 1.0).
+          left-handed = false; # Whether to set the device to left-handed mode.
+        };
+
+        touchpad = {
+          accel-profile = "flat"; # What acceleration profile to use ("adaptive", "flat": disables pointer acceleration).
+          accel-speed = 0; # Pointer acceleration speed (-1.0 - 1.0).
+          disabled-on-external-mouse = false; # Disables touchpad when an external pointer device is plugged in.
+          dwt = false; # Disables touchpad while typing.
+          dwtp = false; # Disables touchpad while using trackpoint.
+          natural-scroll = false; # If enabled, inverts the scrolling direction.
+          scroll-method = "two-finger"; # When to generate scroll events instead of pointer motion events ("no-scroll", "two-finger", "edge", "on-button-down").
+          tap = true; # Enable tap-to-click.
+          click-method = "clickfinger"; # Method to determine which mouse button is pressed when you click the touchpad ("button-areas": Button is determined by which part of the touchpad was clicked, "clickfinger": Button is determined by how many fingers clicked).
+        };
+
+        warp-mouse-to-focus = {
+          enable = true; # Whether the mouse moves to newly focused windows.
+          mode = "center-xy-always"; # Method to determine how the mouse warps ("center-xy": If the mouse was outside the newly focused window, warp the mouse to the center of the window, "center-xy-always": Always warp the mouse to the center of the newly focused window).
+        };
+
+        #workspace-auto-back-and-forth.enable = false; # Focus the previous active workspace if the desired workspace is already focused.
+      };
+
+      # Cursor settings and theming
       cursor = {
         size = config.stylix.cursor.size;
         theme = "${config.stylix.cursor.name}";
       };
 
-      gestures = {
-        hot-corners.enable = false;
-      };
-
-      hotkey-overlay = {
-        skip-at-startup = true;
-        hide-not-bound = false;
-      };
-
-      screenshot-path = "~/Pictures/Screenshots/niri-$(date '+%Y%m%d-%H:%M:%S').png";
-      clipboard.disable-primary = true; # Disable primary selection clipboard (middle mouse paste)
-      prefer-no-csd = true; # Prefer server-side decorations (SSD) over client-side decorations (CSD)
-
+      # Scroller layout
       layout = {
         border = {
-          enable = true;
-          width = 2;
+          enable = true; # Whether borders are drawn.
+          width = 2; # Border width in pixels.
         };
 
-        focus-ring.enable = false;
+        focus-ring.enable = false; # Whether focus ring is drawn.
 
-        shadow.enable = false;
+        shadow.enable = false; # Whether shadows are drawn.
 
-        insert-hint = {
-          enable = true;
-        };
+        insert-hint.enable = true; # Shows where a window will be inserted when you drag and release the window.
 
+        # Widths that `switch-preset-column-width` will cycle through.
         preset-column-widths = [
-          { proportion = 0.25; }
+          { proportion = 0.2; }
           { proportion = 0.5; }
-          { proportion = 0.75; }
-          { proportion = 1.0; }
+          { proportion = 0.8; }
         ];
 
+        # Heights that `switch-preset-window-height` will cycle through.
         preset-window-heights = [
-          { proportion = 0.25; }
+          { proportion = 0.2; }
           { proportion = 0.5; }
-          { proportion = 0.75; }
-          { proportion = 1.0; }
+          { proportion = 0.8; }
         ];
 
-        always-center-single-column = false;
-        center-focused-column = "never"; # Type: "never", "always", "on-overflow"
-        default-column-display = "normal"; # Type: "normal", "tabbed"
+        always-center-single-column = false; # Whether a single column is always centered.
+        center-focused-column = "on-overflow"; # When changing focus, the focused column is automatically centered ("never": If the focused column doesn't fit, it will be aligned to the edges of the screen, "on-overflow": If the focused column doesn't fit, it will be centered on the screen, "always": The focused column will always be centered, even if it was already fully visible).
+        default-column-display = "normal"; # How windows in columns should be displayed by default ("normal": Windows are arranged vertically, "tabbed": Windows are arranged in tabs).
+
+        # The default width for new columns.
         default-column-width = {
           proportion = 0.5;
         };
 
         tab-indicator = {
-          enable = true;
-          hide-when-single-tab = true;
-          place-within-column = true;
-          position = "left"; # Type: "left", "right", "top", "bottom"
+          # Tab indicator settings
+          enable = true; # Whether tab indicators are drawn for tabbed columns.
+          hide-when-single-tab = false; # Whether tab indicators are shown when there is a single window in a tabbed column.
+          place-within-column = true; # Whether the tab indicator is included in column sizing.
+          position = "left"; # The position of the tab indicator relative to the tabbed column ("bottom", "left", "right", "top").
 
-          length.total-proportion = 1.0;
-          corner-radius = 0.0;
-          gap = 2.5;
-          gaps-between-tabs = 2.5;
-          width = 2.0;
+          # Tab indicator theming
+          length.total-proportion = 1.0; # The relative length the tab indicator should take up.
+          corner-radius = 0.0; # Tab corner radius in pixels. If `gaps-between-tabs` is zero, only the first and last tabs have rounded corners.
+          gap = 3.0; # The gap between the tab indicator and the tabbed column in pixels.
+          gaps-between-tabs = 3.0; # The gap between the individual tabs in pixels.
+          width = 2.0; # The thickness of the tab indicator in pixels.
         };
 
-        gaps = 3;
+        gaps = 3; # The gap between windows in the layout, measured in pixels.
 
+        # The distance from the edges of the screen to the edges of the working area.
         struts = {
-          bottom = 6;
-          left = 6;
-          right = 6;
-          top = 6;
+          bottom = 5.5;
+          left = 17.0;
+          right = 17.0;
+          top = 5.5;
         };
       };
 
-      animations = {
-        enable = true;
-      };
+      # Animations
+      animations.enable = false; # Whether to enable animations for windows and workspaces.
 
-      input = {
-        keyboard = {
-          xkb.layout = "us";
-          numlock = true;
-        };
-
-        focus-follows-mouse = {
-          enable = true;
-          max-scroll-amount = "95%";
-        };
-
-        mouse = {
-          accel-profile = "flat";
-          accel-speed = 0;
-          left-handed = false;
-        };
-
-        touchpad = {
-          accel-profile = "flat";
-          accel-speed = 0;
-          disabled-on-external-mouse = false;
-          dwt = false; # Disables touchpad while typing
-          dwtp = false; # Disables touchpad while using trackpoint
-          natural-scroll = false;
-          #scroll-method = "two-finger"; # Type: null, "no-scroll", "two-finger", "edge", "on-button-down"
-          tap = true; # Enable tap-to-click
-        };
-
-        warp-mouse-to-focus.enable = true;
+      # Gesture settings
+      gestures = {
+        hot-corners.enable = false; # Whether moving the mouse to the very top-left corner of a monitor toggles the overview.
       };
     };
   };
