ca8800e2647371923d83056b4ed9195d21ea973b
j <j@test.email>
2026-05-04 00:32:45 -0400

    Moved `workspaces` to its own file.

---
+++ home-manager/niri/workspaces.nix
@@ -0,0 +1,57 @@
+{ ... }:
+
+{
+  config = {
+    programs.niri.settings.workspaces = {
+        "workspace-01" = {
+          name = "1";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-02" = {
+          name = "2";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-03" = {
+          name = "3";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-04" = {
+          name = "4";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-05" = {
+          name = "5";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-06" = {
+          name = "6";
+          open-on-output = "DP-1";
+        };
+
+        "workspace-07" = {
+          name = "7";
+          open-on-output = "DP-2";
+        };
+
+        "workspace-08" = {
+          name = "8";
+          open-on-output = "DP-2";
+        };
+
+        "workspace-09" = {
+          name = "9";
+          open-on-output = "DP-2";
+        };
+
+        "workspace-10" = {
+          name = "10";
+          open-on-output = "DP-2";
+        };
+      };
+    };
+}
