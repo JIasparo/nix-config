2797851ef80b5461bd173880c40751fe8d8bcc25
j <j@test.email>
2026-05-04 00:32:41 -0400

    Moved `workspaces` to its own file. Added window rules.

--- home-manager/niri/rules.nix
+++ home-manager/niri/rules.nix
@@ -1,50 +1,105 @@
-{ ... }:
+{ config, ... }:
 
 {
   config = {
     programs.niri.settings = {
-      spawn-at-startup = [
-        #{ argv = [ "bitwarden" ]; }
-      ];
-
-      workspaces = {
-        "workspace-1" = {
-          name = "1";
-          open-on-output = "DP-2";
-        };
-
-        "workspace-2" = {
-          name = "2";
-          open-on-output = "DP-2";
-        };
-
-        "workspace-3" = {
-          name = "3";
-          open-on-output = "DP-2";
-        };
-
-        "workspace-4" = {
-          name = "4";
-          open-on-output = "DP-1";
-        };
-
-        "workspace-5" = {
-          name = "5";
-          open-on-output = "DP-1";
-        };
-
-        "workspace-6" = {
-          name = "6";
-          open-on-output = "DP-1";
-        };
-      };
-
+      # niri msg windows # List all open windows.
+      
       window-rules = [
-        #
+        # Game launcher rules
+        {
+          open-on-workspace = "${config.programs.niri.settings.workspaces.workspace-04.name}";
+          open-focused = true;
+
+          matches = [
+            { app-id = "^(steam)$"; }
+            {
+              app-id = "^(electron)$";
+              title = "^(Heroic Games Launcher)$";
+            }
+          ];
+        }
+
+        # Game rules
+        {
+          open-on-workspace = "${config.programs.niri.settings.workspaces.workspace-05.name}";
+          open-focused = true;
+          variable-refresh-rate = true; # Whether windows use VRR when displayed on an output with `on-demand` VRR.
+
+          matches = [
+            { app-id = "^(steam_app_\\d+)$"; }
+            { app-id = "^(cs2)$"; }
+          ];
+        }
+
+        # Instant messenger rules
+        {
+          open-on-workspace = "${config.programs.niri.settings.workspaces.workspace-08.name}";
+          open-focused = true;
+
+          matches = [
+            { app-id = "^(equibop)$"; }
+          ];
+        }
+
+        # Multimedia rules
+        {
+          #
+
+          matches = [
+            { app-id = "^(vlc)$"; }
+          ];
+        }
+
+        # Recording software rules
+        {
+          open-on-workspace = "${config.programs.niri.settings.workspaces.workspace-09.name}";
+          open-focused = true;
+
+          matches = [
+            { app-id = "^(com.obsproject.Studio)$"; }
+          ];
+        }
+
+        # Screen recording window rules
+        {
+          border = {
+            active.color = "#B32834";
+            inactive.color = "#792F35";
+          };
+
+          focus-ring = {
+            active.color = "#B32834";
+            inactive.color = "#792F35";
+          };
+
+          shadow.color = "#792F35";
+
+          matches = [
+            { is-window-cast-target = true; }
+          ];
+        }
+
+        # Sensitive window rules
+        {
+          block-out-from = "screencast"; # ("screencast": Window cannot be seen by screen recorders but can be seen by screenshots, "screen-capture": Window cannot be seen by screen recorders or screenshots).
+
+          matches = [
+            { app-id = "^(Bitwarden)$"; }
+          ];
+        }
       ];
 
+      # niri msg layers # List all open layer-shell surfaces.
       layer-rules = [
-        #
+        # Sensitive layer rules
+        {
+          block-out-from = "screencast"; # ("screencast", "screen-capture").
+
+          matches = [
+            { namespace = "^(swaync-notification-window)$"; }
+          ];
+        }
       ];
     };
   };
