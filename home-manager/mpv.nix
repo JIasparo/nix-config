f65d2503057d571b345ef733edd23c11c81c680a
j <j@test.email>
2026-05-23 01:05:10 -0400

    .

--- home-manager/mpv.nix
+++ home-manager/mpv.nix
@@ -8,6 +8,13 @@
       scripts = with pkgs.mpvScripts; [
         #
       ];
+
+      config = {
+        #profile = "gpu-hq";
+        #force-window = true;
+        #ytdl-format = "bestvideo+bestaudio";
+        #cache-default = 4000000;
+      };
     };
   };
 }
