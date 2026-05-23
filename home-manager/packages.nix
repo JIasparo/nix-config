eab0b3cdaf85215ea68a133fcf7dbede3eac60b2
j <j@test.email>
2026-05-23 01:06:05 -0400

    added bottles with an override to skip broken openldap check

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -8,6 +8,31 @@
       blender
       #(blender.override { config.cudaSupport = true; config.rocmSupport=true; }) # Blender with GPU support.
       #(bottles.override { removeWarningPopup = true; })
+      (pkgs.bottles.override {
+        removeWarningPopup = true;
+        
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv =
+          args:
+          pkgs.buildFHSEnv (
+            args
+            // {
+              multiPkgs =
+                envPkgs:
+                let
+                  # Fetch original package list
+                  originalPkgs = args.multiPkgs envPkgs;
+
+                  # Disable tests for openldap
+                  customLdap = envPkgs.openldap.overrideAttrs (_: {
+                    doCheck = false;
+                  });
+                in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+            }
+          );
+      })
       brightnessctl
       #chatterino2
       #davinci-resolve
@@ -25,6 +50,7 @@
       unrar
       vlc
       wl-clipboard
+      sm64coopdx
     ];
   };
 }
