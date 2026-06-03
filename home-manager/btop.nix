6a6bd87545bb20fe991a1e97c97476e73f6f2a4a
j <j@test.email>
2026-06-03 10:43:50 -0400

    updated settings. Added `proc_sorting = "memory";`

--- home-manager/btop.nix
+++ home-manager/btop.nix
@@ -7,7 +7,8 @@
       package = pkgs.btop-cuda; # btop-cuda for Nvidia GPUs | btop-rocm for AMD GPUs
 
       settings = {
-        shown_boxes = "proc cpu mem net gpu0";
+        proc_sorting = "memory"; # Processes sorting ("pid", "program", "arguments", "threads", "user", "memory", "cpu lazy": Sorts top process over time, "cpu direct": Updates top process directly).
+        shown_boxes = "proc cpu mem net gpu0"; # Manually set which boxes to show.
       };
     };
   };
