cfbdf98cbdbaa5e5bfcb2a0e7a5b158774f9f4af
j <j@test.email>
2026-06-07 16:47:14 -0400

    removed unnecessary bottles override (the failed openldap check was fixed)

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -4,35 +4,10 @@
   config = {
     home.packages = with pkgs; [
       #archipelago
-      bitwarden-desktop
+      #bitwarden-desktop # Uses outdated version of Electron
       blender
       #(blender.override { config.cudaSupport = true; config.rocmSupport=true; }) # Blender with GPU support.
-      #(bottles.override { removeWarningPopup = true; })
-      (pkgs.bottles.override {
-        removeWarningPopup = true;
-        
-        # Intercept buildFHSEnv to modify target packages
-        buildFHSEnv =
-          args:
-          pkgs.buildFHSEnv (
-            args
-            // {
-              multiPkgs =
-                envPkgs:
-                let
-                  # Fetch original package list
-                  originalPkgs = args.multiPkgs envPkgs;
-
-                  # Disable tests for openldap
-                  customLdap = envPkgs.openldap.overrideAttrs (_: {
-                    doCheck = false;
-                  });
-                in
-                # Replace broken openldap with the custom one
-                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
-            }
-          );
-      })
+      (bottles.override { removeWarningPopup = true; })
       brightnessctl
       #chatterino2
       #davinci-resolve
