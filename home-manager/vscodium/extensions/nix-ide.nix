1ab17f1a13394417f40f442f0915e78d30abd89c
val <val@test.email>
2026-09-13 19:32:10 -0400

    .

--- home-manager/vscodium/extensions/nix-ide.nix
+++ home-manager/vscodium/extensions/nix-ide.nix
@@ -1,6 +1,6 @@
 {
-  lib,
   pkgs,
+  lib,
   configpath,
   hostname,
   username,
@@ -9,31 +9,36 @@
 
 {
   config = {
-    programs.vscodium.profiles."default" = {
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-      ];
+    programs.vscodium.profiles."default" =
+      let
+        language-server = pkgs.nixd;
+        formatter = pkgs.nixfmt;
+      in
+      {
+        extensions = with pkgs.vscode-extensions; [
+          jnoortheen.nix-ide
+        ];
 
-      userSettings = {
-        "nix.enableLanguageServer" = true;
-        "nix.hiddenLanguageServerErrors" = [ "textDocument/definition" ];
-        "nix.serverPath" = "${lib.getExe pkgs.nixd}";
-        "nix.serverSettings" = {
-          "${lib.getName pkgs.nixd}" = {
-            "formatting" = {
-              "command" = [ "${lib.getExe pkgs.nixfmt}" ];
-            };
-            "options" = {
-              "nixos" = {
-                "expr" = "(builtins.getFlake \"${configpath}\").nixosConfigurations.${hostname}.options";
+        userSettings = {
+          "nix.enableLanguageServer" = true;
+          "nix.hiddenLanguageServerErrors" = [ "textDocument/definition" ];
+          "nix.serverPath" = "${lib.getExe language-server}";
+          "nix.serverSettings" = {
+            "${lib.getName language-server}" = {
+              "formatting" = {
+                "command" = [ "${lib.getExe formatter}" ];
               };
-              "home-manager" = {
-                "expr" = "(builtins.getFlake \"${configpath}\").homeConfigurations.${username}.options";
+              "options" = {
+                "nixos" = {
+                  "expr" = "(builtins.getFlake \"${configpath}\").nixosConfigurations.${hostname}.options";
+                };
+                "home-manager" = {
+                  "expr" = "(builtins.getFlake \"${configpath}\").homeConfigurations.${username}.options";
+                };
               };
             };
           };
         };
       };
-    };
   };
 }
